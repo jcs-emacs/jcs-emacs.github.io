6522c9a1831d17646315c5cf40ea8c365147118e
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-26 03:35:08 +0800

    Add banner

---
+++ src/components/EBanner.js
@@ -0,0 +1,18 @@
+/**
+ * $File: EBanner.js $
+ * $Date: 2022-01-26 03:34:33 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2022 by Shen, Jen-Chieh $
+ */
+import React from 'react';
+
+class Ebanner extends React.Component {
+  state = {};
+  render () {
+    return (<></>);
+  }
+}
+
+export default Ebanner;
