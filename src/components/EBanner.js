5d3afdeb2f64b91dec7e52d50bca793e3e2cf5f6
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-26 04:32:17 +0800

    Update impls

--- src/components/EBanner.js
+++ src/components/EBanner.js
@@ -7,11 +7,44 @@
  *                   Copyright © 2022 by Shen, Jen-Chieh $
  */
 import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+
+import banner from '../etc/sink_white.png';
+
+import { FiDownload } from 'react-icons/fi';
+import { GrInstall } from 'react-icons/gr';
 
 class Ebanner extends React.Component {
   state = {};
   render () {
-    return (<></>);
+    return (
+      <>
+        <div className="text-center">
+          <div>
+            <img src= { banner }/>
+          </div>
+          <br/>
+          <div className="text-info h5">
+            [J C S • E M A C S]
+          </div>
+          <div className="text-white-50 h6 p-1">
+            Emacs configuration works across all OSs
+          </div>
+          <br/><br/>
+          <Button variant="light" className="rounded-pill border-2 border-success btn-lg"
+                  href="https://github.com/jcs-emacs/jcs-emacs/archive/refs/heads/master.zip">
+            <FiDownload  /> Download
+          </Button>{' '}
+          <Button variant="dark" className="rounded-pill border-2 border-primary btn-lg"
+                  href="#collapseExample"
+                  type="button"
+                  data-toggle="collapse"
+                  role="button" aria-expanded="false" aria-controls="collapseExample">
+            <GrInstall /> Install
+          </Button>
+          <br/><br/>
+        </div>
+      </>);
   }
 }
 
--- src/components/EFooter.js
+++ src/components/EFooter.js
@@ -14,9 +14,18 @@ class EFooter extends React.Component {
   render () {
     return (
       <>
-        <Container variant="mt-auto">
-          Copyright © 2022
-        </Container>
+        <footer className="mt-auto text-center text-white p-4">
+          Copyright © 2022{' '}
+          <a className="text-reset fw-bold" href="https://jcs-emacs.github.io/">
+            jcs-emacs.github.io
+          </a>
+          <br/>
+          This website is licensed under the{' '}
+          <a href="https://creativecommons.org/licenses/by-nd/4.0/">
+            CC BY-ND 4.0
+          </a>
+          International License.
+        </footer>
       </>);
   }
 }
