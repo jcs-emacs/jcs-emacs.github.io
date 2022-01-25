aae6b6d04b19d710c7613abb0e0ecc20bc2665ab
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-25 19:35:35 +0800

    Update title

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import ENavbar from './components/ENavbar.js';
 import EFooter from './components/EFooter.js';
 
 class App extends React.Component {
+  constructor() {
+    document.title = 'J C S • E M A C S';
+  }
+
   render() {
     return (
       <React.Fragment>
