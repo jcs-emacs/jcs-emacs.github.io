26a770dd333d80d4ab051c72f02e1acfce19b003
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-26 17:32:02 +0800

    Update spacing

--- src/components/EBanner.js
+++ src/components/EBanner.js
@@ -7,12 +7,12 @@
  *                   Copyright © 2022 by Shen, Jen-Chieh $
  */
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 
 import banner from '../etc/sink_white.png';
 
 import { FiDownload } from 'react-icons/fi';
-import { GrInstall } from 'react-icons/gr';
+import { RiInstallFill } from 'react-icons/ri';
 
 class Ebanner extends React.Component {
   state = {};
@@ -20,29 +20,30 @@ class Ebanner extends React.Component {
     return (
       <>
         <div className="text-center">
-          <div>
+          <div className="p-4">
             <img src= { banner } alt="banner" />
           </div>
-          <br/>
           <div className="text-info h5">
             [J C S • E M A C S]
           </div>
           <div className="text-white-50 h6 p-1">
             Emacs configuration works across all OSs
           </div>
-          <br/><br/>
-          <Button variant="light" className="rounded-pill border-2 border-success btn-lg"
-                  href="https://github.com/jcs-emacs/jcs-emacs/archive/refs/heads/master.zip">
-            <FiDownload  /> Download
-          </Button>{' '}
-          <Button variant="dark" className="rounded-pill border-2 border-primary btn-lg"
-                  href="#collapseExample"
-                  type="button"
-                  data-toggle="collapse"
-                  role="button" aria-expanded="false" aria-controls="collapseExample">
-            <GrInstall /> Install
-          </Button>
-          <br/><br/>
+          <Container className="p-4">
+            <Button variant="dark"
+                    className="rounded-pill btn-lg border-2 border-success px-4 mx-1"
+                    href="https://github.com/jcs-emacs/jcs-emacs/archive/refs/heads/master.zip">
+              <FiDownload  /> Download
+            </Button>{' '}
+            <Button variant="dark"
+                    className="rounded-pill btn-lg border-2 border-success px-5 mx-1"
+                    href="#collapseExample"
+                    type="button"
+                    data-toggle="collapse"
+                    role="button" aria-expanded="false" aria-controls="collapseExample">
+              <RiInstallFill /> Install
+            </Button>
+          </Container>
         </div>
       </>);
   }
